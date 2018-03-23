01052ba49ff80f07f93b98453f3b03422ca5491c
Kathryn Brusewitz <kathryn.brusewitz@gmail.com>
2018-03-22 23:58:48 -0700

    clean up

--- clock.js
+++ clock.js
@@ -7,7 +7,7 @@
  * @param {Number} y canvas
  * @param {Number} radius
  */
-var Point = function(x, y, radius) {
+const Point = (x, y, radius) => {
   this.x = x;
   this.y = y;
   this.radius = radius;
@@ -23,23 +23,23 @@ var Point = function(x, y, radius) {
  * @param {Number} margin
  * @param {Array.<String>} color
  */
-var PolarClock = function (canvas, point, line, margin, color) {
+const PolarClock = (canvas, point, line, margin, color) => {
   this.canvas = canvas;
-  this.context = canvas.getContext("2d");
+  this.context = canvas.getContext('2d');
   this.point = point;
   this.line = line;
   this.margin = margin;
-  this.color = color || ["#333", "#555", "#777", "#000", "#BBB", "#999"];
+  this.color = color || ['#333', '#555', '#777', '#000', '#BBB', '#999'];
 };
 
 /**
  * @param {Number} interval (ms)
  */
-PolarClock.prototype.start = function(interval) {
-  var self = this;
-  var point = this.getPoint();
+PolarClock.prototype.start = (interval) => {
+  const self = this;
+  const point = this.getPoint();
 
-  setInterval(function() {
+  setInterval(() => {
     self.step(point);
   }, interval);
 };
@@ -47,25 +47,60 @@ PolarClock.prototype.start = function(interval) {
 /**
  * clear
  */
-PolarClock.prototype.clear = function() {
+PolarClock.prototype.clear = () => {
   this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 };
 
 /**
  * @return {Point}
  */
-PolarClock.prototype.getPoint = function() {
+PolarClock.prototype.getPoint = () => {
   return new Point(this.point.x, this.point.y, this.point.radius);
 };
 
 /**
+ * end of months for feb
+ */
+const getEndOfMonth = (date) => {
+  const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const year = date.getYear();
+  const month = date.getMonth();
+
+  return (month === 1 && year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) ? 29 : days[month];
+};
+
+
+/**
+ * time radian
+ */
+const getTimeRadian = () => {
+  const now = new Date();
+  const eom = getEndOfMonth(now);
+
+  const second = (now.getSeconds() + now.getMilliseconds() / 1000) * Math.PI / 30;
+  const minute = (now.getMinutes() * Math.PI / 30) + second / 60;
+  const hour = (now.getHours() * Math.PI / 12) + minute / 24;
+  const weekday = (now.getDay() * Math.PI / 3.5) + hour / 7;
+  const date = ((now.getDate() - 1) * Math.PI / (eom/2)) + hour / eom;
+  const month = (now.getMonth() * Math.PI / 6) + date / 12;
+
+  return {
+    second,
+    minute,
+    hour,
+    weekday,
+    date,
+    month,
+  };
+};
+
+
+/**
  * @param {Point} point
  */
-PolarClock.prototype.step = function(point) {
-  var now = getTimeRadian();
-  var line = this.line;
-  var margin = this.margin;
-  var color = this.color;
+PolarClock.prototype.step = (point) => {
+  const now = getTimeRadian();
+  const color = this.color;
 
   this.clear();
   if (color[0]) this.draw(0, point, now.second);
@@ -81,11 +116,11 @@ PolarClock.prototype.step = function(point) {
  * @param {Point} point
  * @param {Number} radian
  */
-PolarClock.prototype.draw = function(index, point, radian) {
+PolarClock.prototype.draw = (index, point, radian) => {
   this.point = new Point(
     point.x,
     point.y,
-    point.radius - (this.line + this.margin) * index
+    point.radius - (this.line + this.margin) * index,
   );
 
   this.arc(this.color[index], this.line, 0, radian);
@@ -97,62 +132,19 @@ PolarClock.prototype.draw = function(index, point, radian) {
  * @param {Number} start
  * @param {Number} end
  */
-PolarClock.prototype.arc = function(color, width, start, end) {
-  var context = this.context;
-  var point = this.point;
-  var x = -point.y;
-  var y = point.x;
-  var r = point.radius-width;
+PolarClock.prototype.arc = (color, width, start, end) => {
+  const context = this.context;
+  const point = this.point;
+  const x = -point.y;
+  const y = point.x;
+  const r = point.radius - width;
 
   context.save();
-  context.rotate(-Math.PI/2);
+  context.rotate(-Math.PI / 2);
   context.strokeStyle = color;
   context.lineWidth = width;
   context.beginPath();
   context.arc(x, y, r, start, end, false);
   context.stroke();
   context.restore();
-};
-
-//--------------------------------------------------------------------------
-//  Private methods
-//--------------------------------------------------------------------------
-/**
- * time radian
- */
-var getTimeRadian = function() {
-  var now = new Date();
-  var eom = getEndOfMonth(now);
-
-  var second = (now.getSeconds() + now.getMilliseconds() / 1000) * Math.PI / 30;
-  var minute = (now.getMinutes() * Math.PI / 30) + second / 60;
-  var hour = (now.getHours() * Math.PI / 12) + minute / 24;
-  var weekday = (now.getDay() * Math.PI / 3.5) + hour / 7;
-  var date = ((now.getDate() - 1) * Math.PI / (eom/2)) + hour / eom;
-  var month = (now.getMonth() * Math.PI / 6) + date / 12;
-
-  return {
-    second: second,
-    minute: minute,
-    hour: hour,
-    weekday: weekday,
-    date: date,
-    month: month
-  };
-};
-
-/**
- * end of months for feb
- */
-var getEndOfMonth = function(date) {
-  var eom;
-  var days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  var year = date.getYear();
-  var month = date.getMonth();
-
-  if (month == 1 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-    return 29;
-  } else {
-    return days[month];
-  }
 };
